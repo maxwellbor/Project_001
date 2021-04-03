c0d113fd82f59f7f197f501b33c919d0a9efd65f
Maxwell <maxwellbor@hotmail.com>
2021-04-03 13:49:51 -0300

    Movimentos Basicos

--- Player.gd
+++ Player.gd
@@ -10,5 +10,20 @@ func _physics_process(delta):
 	# Limitar a Aceleração da Gravidade
 	if motion.y < 200:
 		motion.y += 20
+	
+	# Pular
+	# Pular apenas em contato com o Chão
+	if Input.is_action_pressed("ui_up") and is_on_floor():
+		motion.y = -500
+	# Mover para Direita
+	elif Input.is_action_pressed("ui_right"):
+		motion.x = 100
+	# Mover para Esquerda
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -100
+	# Para movimento Horizontal
+	else:
+		motion.x = 0
+	
 	# Executar Movimentação
 	move_and_slide(motion, floorDirection)
